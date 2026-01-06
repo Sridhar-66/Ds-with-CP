822fde207f68c9e9475c2946a35f0031e6a6250b
Sridhar <241801340016@cutmap.ac.in>
2026-01-06 12:20:04 +0530

    Add Node class with methods to get data and reference

--- 6jan.java
+++ 6jan.java
@@ -18,3 +18,34 @@ class Main {
         System.out.println(r);
     }
 }
+class Node{
+    int data;
+    Node next;
+    //constructor
+    Node(int a){
+        data=a;
+        next=null;
+    }
+    
+    //method get data
+    public void getdata(){
+        System.out.println(data);
+    }
+    public void getref(){
+        System.out.println(next);
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        System.out.println("ajay");
+        Node b=new Node(100);
+        b.getdata();
+        Node c=new Node(44);
+        c.getdata();
+        Node d=new Node(100);
+        d.getdata();
+        Node e=new Node(100);
+        e.getdata();
+    
+    }
+}
